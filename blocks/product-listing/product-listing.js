5d8f0b0b400bc9fd6db152f7825f87e06861dc4a
KiranKM3836 <kirankumarm@klizer.com>
2025-06-05 19:08:34 +0530

    Magento Products Implementation

---
+++ blocks/product-listing/product-listing.js
@@ -0,0 +1,24 @@
+export default async function decorate(block) {
+    const res = await fetch('https://mesh.rdklizer.com/rest/V1/products/?searchCriteria[pageSize]=12', {
+      headers: {
+        'Authorization': 'Bearer eyJraWQiOiIxIiwiYWxnIjoiSFMyNTYifQ.eyJ1aWQiOjEsInV0eXBpZCI6MiwiaWF0IjoxNzQ5MDIzMDQwLCJleHAiOjE3NDkwMjY2NDB9.tc_-5j7DARoscIhu506j-ejmqyRVzGgeddzTCctIy4A'
+      }
+    });
+  
+    const { items } = await res.json();
+
+    console.log(items,"items")
+  
+    block.innerHTML = `
+      <div class="plp-grid">
+        ${items.map(p => `
+          <a class="product-card" href="/product/?sku=${p.sku}">
+            <img src="https://mesh.rdklizer.com/media/catalog/product${p.media_gallery_entries[0]?.file}" alt="${p.name}" />
+            <h3>${p.name}</h3>
+            <p>$${p.price}</p>
+          </a>
+        `).join('')}
+      </div>
+    `;
+  }
+  
